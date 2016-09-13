51c0bb78955632a63169f2559764291cfeebcead
DLX <denver@ii.org.nz>
2016-09-13 20:54:27 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone --branch chip-fido https://gitlab.ii.org.nz/iichip/poky.git /home/build/poky
+cd /home/build/poky
 source oe-init-build-env build-chip
 bitbake chip-hwup-image
